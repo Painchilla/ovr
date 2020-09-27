017e6a5c7a9ceae0a3a07831bb00cd0440414ef0
weingartz <henrik+git@weingartz.eu>
2020-09-27 11:05:02 +0200

    Added Streamkey-update

--- node_server.js
+++ node_server.js
@@ -1,6 +1,27 @@
 var http = require('http');
+var url = require('url');
+var streamkey='THIS IS NOT A VALID STREAMKEY';
 
 http.createServer(function(req,res) {
-    res.write('Hello World');
+    console.log('New Request Incoming')
+    var requrl= url.parse(req.url,true);
+    if(requrl.pathname==='/streamkey'){
+        if(req.method==='POST'){
+            var params = requrl.query;
+            if(params.streamkey !== undefined) {
+                console.log('\t' + 'New Streamkey:' + params.streamkey);
+                streamkey=params.streamkey;
+                res.write(streamkey + '\n');
+            } else {
+                res.write('\t' +'Please provide a streamkey with a post request.' + "\n");
+            }
+            
+        } else{
+            console.log('\t' +"Streamkey was accessed by" + req._connectionKey);
+            res.write(streamkey);
+        }
+    } else {
+        res.write(req.method + "\n");
+    }
     res.end();
 }).listen(8080);
